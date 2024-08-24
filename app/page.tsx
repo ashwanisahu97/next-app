9f83a9517ab3b373700daf4e0e3c750723d4e3ab
Ashwani Sahu <ashwani.s@honeysys.com>
2024-08-24 20:45:54 +0530

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Hello World!</h1>
+      <Link href="/user">user</Link>
+    </main>
+  );
+}
