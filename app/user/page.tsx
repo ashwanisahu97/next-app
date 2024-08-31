806c74e39c0e8642fdb24b07bc1ec4ef41ffe193
Ashwani Sahu <ashwani.s@honeysys.com>
2024-08-31 18:06:22 +0530

    tailwind.css added properly

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
-
-const UserPage = () => {
-  return (
-    <div>UserPage</div>
-  )
+import React from "react";
+interface User{
+  id:number;
+  name:string;
 }
+const UserPage = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users",{next:{revalidate:10}});
+  const users:User[] = await response.json();
+  return(
+    <div className='m-10 text-white'>
+      <h1 className='btn-active'>Users</h1>
+      <p>{new Date().toLocaleTimeString()}</p>
+      <ul className="p-4 bg-sky-400 text-white">
+        {users?.map(user=><li key={user?.id}>{user?.name}</li>)}
+      </ul>
+    </div>
+  );
+};
 
-export default UserPage
+export default UserPage;
